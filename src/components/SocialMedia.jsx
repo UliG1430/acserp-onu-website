60047f61839a125f23d6326db8b1b0ab3f85c1a4
Ulises Geymonat <ulisesgeymonat@Laptop-de-Ulises.local>
2024-08-24 22:45:30 -0300

    not working yet

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,10 +1,10 @@
 import instagramIcon from '../assets/images/icon-instagram.png';
-import tiktokIcon from '../assets/images/icon-tiktok.png';
-import linkedinIcon from '../assets/images/icon-linkedin.png';
-import emailIcon from '../assets/images/icon-email.png';
-import youtubeIcon from '../assets/images/icon-youtube.png';
-import twitterIcon from '../assets/images/icon-twitter.png';
-import facebookIcon from '../assets/images/icon-facebook.png';
+// import tiktokIcon from '../assets/images/icon-tiktok.png';
+// import linkedinIcon from '../assets/images/icon-linkedin.png';
+// import emailIcon from '../assets/images/icon-email.png';
+// import youtubeIcon from '../assets/images/icon-youtube.png';
+// import twitterIcon from '../assets/images/icon-twitter.png';
+// import facebookIcon from '../assets/images/icon-facebook.png';
 
 function SocialMedia() {
   return (
@@ -12,7 +12,7 @@ function SocialMedia() {
       <a href="https://www.instagram.com/your_instagram_handle/" target="_blank" rel="noopener noreferrer">
         <img src={instagramIcon} alt="Instagram" className="w-8 h-8" />
       </a>
-      <a href="https://www.tiktok.com/@your_tiktok_handle" target="_blank" rel="noopener noreferrer">
+      {/* <a href="https://www.tiktok.com/@your_tiktok_handle" target="_blank" rel="noopener noreferrer">
         <img src={tiktokIcon} alt="TikTok" className="w-8 h-8" />
       </a>
       <a href="https://www.linkedin.com/in/your_linkedin_handle/" target="_blank" rel="noopener noreferrer">
@@ -29,7 +29,7 @@ function SocialMedia() {
       </a>
       <a href="https://www.facebook.com/your_facebook_page" target="_blank" rel="noopener noreferrer">
         <img src={facebookIcon} alt="Facebook" className="w-8 h-8" />
-      </a>
+      </a> */}
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // Esto abre automáticamente el navegador cuando inicies el servidor
+  }
+});
