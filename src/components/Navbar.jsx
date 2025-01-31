3eb8adcb337baebbe2d8bb5c7bf259f7088cb8e0
ugeymonat <ugeymonat@ec.gba.gov.ar>
2025-01-31 13:50:38 -0300

    fix carrusel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-950 shadow-lg sticky top-0 z-10">
+    <nav className="bg-[#121445] shadow-lg sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Botón hamburguesa para móvil */}
